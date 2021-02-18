bf518336c862ecbe8c318fc42c2a93d93076c75c
BeggiNN <49918901+BeggiNN@users.noreply.github.com>
2021-02-18 16:00:05 +0200

    Add files via upload

---
+++ CurrectProduct/Block/Custom.php
@@ -0,0 +1,25 @@
+<?php
+namespace Perspective\CurrectProduct\Block;
+class Custom extends \Magento\Framework\View\Element\Template
+{
+    protected $_stockItemRepository;
+
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\CatalogInventory\Model\Stock\StockItemRepository $stockItemRepository,
+        array $data = []
+    )
+    {
+        $this->_stockItemRepository = $stockItemRepository;
+        parent::__construct($context, $data);
+    }
+
+    public function getStockItem($productId)
+    {
+        return $this->_stockItemRepository->get($productId);
+    }
+}
+
+?>
+
+
